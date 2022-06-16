c9a8e0971e4e08fe48909413c1753ca8a18cf546
vhs <90valentin@gmail.com>
2022-06-16 18:29:11 +0200

    gitignore

--- user.nix
+++ user.nix
@@ -1,16 +1,14 @@
-let
-  user = (import ../user.nix); in
 {
-  efiBoot = user.efiBoot;
-  mbrDevice = user.mbrDevice;
-  email = user.email;
-  handle = user.handle;
-  theme = user.theme;
-  foreground = user.foreground;
-  font = user.font;
-  secondaryFont = user.secondaryFont;
-  liberaPass = user.liberaPass;
-  liberaUser = user.liberaUser;
-  pathToHomeManagerConfigDir = user.pathToHomeManagerConfigDir;
-  withgtk = user.withgtk;
+  efiBoot = false;
+  mbrDevice = "/dev/sda";
+  email = "90valentin@gmail.com";
+  handle = "vhsconnect";
+  theme = "rebecca";
+  foreground = "light";
+  font = "VictorMono Nerd Font";
+  secondaryFont = "VictorMono Nerd Font";
+  liberaPass = "";
+  liberaUser = "";
+  pathToHomeManagerConfigDir = "./nixos/homemanager/nixpkgs";
+  withgtk = false;
 }
