7313f7bbdff3a13c9db4f91d84d129bda0ef81cf
vhsconnect <90valentin@gmail.com>
2022-10-05 08:18:38 +0200

    mpu3 extra drive

--- hardware/mpu3/hardware-configuration.nix
+++ hardware/mpu3/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
@@ -14,18 +15,26 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1db1b002-b984-4b1d-92af-8156c2b87247";
+    {
+      device = "/dev/disk/by-uuid/1db1b002-b984-4b1d-92af-8156c2b87247";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8C09-582A";
+    {
+      device = "/dev/disk/by-uuid/8C09-582A";
       fsType = "vfat";
     };
 
+  fileSystems."/home/vhs/Data" =
+    {
+      device = "/dev/disk/by-uuid/d536bf84-da2d-49ad-83df-710d3d0daf23";
+      fsType = "ext4";
+    };
+
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/fb2fd9c3-0a5a-4de6-b05d-ed44f2589cab"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/fb2fd9c3-0a5a-4de6-b05d-ed44f2589cab"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
