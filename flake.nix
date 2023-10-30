ca8e35439e43f1aa81b3b54d0df9a1f483d97337
vhsconnect <90valentin@gmail.com>
2023-10-30 01:12:06 +0100

    remove rs-deploy and flake-utils

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
     nixpkgs-2111.url = "github:NixOS/nixpkgs/nixos-21.11";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    deploy-rs.url = "github:serokell/deploy-rs";
-    flake-utils.url = "github:numtide/flake-utils";
 
     darwinNixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.05-darwin";
     darwin.url = "github:lnl7/nix-darwin/master";
@@ -18,17 +16,15 @@
     let
       systems = [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" ];
       genAttrs = inputs.nixpkgs.lib.attrsets.genAttrs;
+      legacyPackages = inputs.nixpkgs.legacyPackages;
       fold = builtins.foldl';
       map = builtins.map;
 
     in
 
     {
-      formatter =
-        (
-          genAttrs systems
-            (x: inputs.nixpkgs.legacyPackages.${x}.nixpkgs-fmt)
-        );
+      formatter = genAttrs systems
+        (x: legacyPackages.${x}.nixpkgs-fmt);
 
       darwinConfigurations = {
         macv = inputs.darwin.lib.darwinSystem
@@ -50,55 +46,23 @@
 
       };
 
-      # devShells =
-      #   inputs.flake-utils.lib.eachSystem systems (system:
-      #     let
-      #       mkShell = inputs.nixpkgs.legacyPackages.${system}.mkShell;
-      #       git-crypt = inputs.nixpkgs.legacyPackages.${system}.git-crypt;
-      #     in
-      #     {
-      #       default =
-      #         mkShell {
-      #           buildInputs = [ git-crypt ];
-      #         };
-      #     }
-      #   );
 
-      # devShells."x86_64-linux" =
-      #   {
-      #     default =
-      #       inputs.nixpkgs.legacyPackages."x86_64-linux".mkShell {
-      #         buildInputs = [ inputs.nixpkgs.legacyPackages."x86_64-linux".git-crypt ];
-      #       };
-      #   };
-
-      devShells = (fold
-        (a: b: a // b)
-        { }
-        (map
-          (x: {
-            ${x}.default =
-              inputs.nixpkgs.legacyPackages.${x}.mkShell {
-                buildInputs = [ inputs.nixpkgs.legacyPackages.${x}.git-crypt ];
-              };
-          })
-          systems)
-      );
+      devShells =
+        let
+          mapSystems = x: map x systems;
+          mergeAttributeSets = fold (a: b: a // b) { };
+        in
+        mergeAttributeSets
+          (mapSystems
+            (x: {
+              ${x}.default =
+                with legacyPackages.${x};
+                mkShell {
+                  buildInputs = [ git-crypt ];
+                };
+            })
+          );
 
 
-
-
-      deploy.nodes.munin = {
-        profiles.system = {
-          user = "root";
-          path =
-            inputs.deploy-rs.lib.x86_64-linux.activate.nixos
-              inputs.self.nixosConfigurations.munin;
-
-        };
-        sshUser = "vhs";
-        hostname = "192.168.1.164";
-        remoteBuild = true;
-      };
     };
 }
