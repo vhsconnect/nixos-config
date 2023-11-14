5c97b50cb6a8ec46ada65fd9926a0f44ba7da5d3
vhsconnect <90valentin@gmail.com>
2023-11-14 17:59:20 +0100

    theme: tokyonight

---
+++ homemanager/themes/tokyonight.nix
@@ -0,0 +1,17 @@
+let
+  white = "#F9F9F9";
+  black = "#282430";
+  compose = f: g: x: f (g x);
+  file = ./tokyonight.json;
+  colors = compose builtins.fromJSON builtins.readFile file;
+in
+with colors; {
+  main = base03;
+  secondary = base01;
+  accent = base01;
+  accent2 = base0C;
+  accent3 = base0C;
+  urgent = base0E;
+  grey = base04;
+  debug = base05;
+}
