d93a70c55ed8e57b666a86090d9211c99413571b
vhsconnect <90valentin@gmail.com>
2023-11-20 20:41:31 +0100

    mbison: add swap

--- hardware/mbison/hardware-configuration.nix
+++ hardware/mbison/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
@@ -14,16 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/eae8ee16-4b29-44ce-95b7-3d98f809d467";
+    {
+      device = "/dev/disk/by-uuid/eae8ee16-4b29-44ce-95b7-3d98f809d467";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EBEF-3A07";
+    {
+      device = "/dev/disk/by-uuid/EBEF-3A07";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/cb67977c-4cb4-430f-b0a0-0d473afbcc13"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
