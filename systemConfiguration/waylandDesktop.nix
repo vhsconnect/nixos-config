b05c923fc591aae9de39ec3a22d5313ac55d7da0
vhsconnect <90valentin@gmail.com>
2026-06-07 19:34:10 +0200

    kanshi

--- systemConfiguration/waylandDesktop.nix
+++ systemConfiguration/waylandDesktop.nix
@@ -14,6 +14,8 @@
   xdg.portal.wlr.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
+  environment.systemPackages = with pkgs; [ kanshi ];
+
   environment.sessionVariables.QML_IMPORT_PATH = lib.makeSearchPath "lib/qt-6/qml" [
     pkgs.kdePackages.qt5compat
     pkgs.kdePackages.qtbase
